750b20ed799419d4c39443e5c15c311deaea198e
Kazuhide Takahashi <kazuhide_takahashi@trustbank.co.jp>
2025-01-17 13:28:30 +0900

    init and ci.yml (step3) stan error fix 3

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -8,9 +8,6 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     // public function testThatTrueIsTrue(): void
     // {
     //     $this->assertTrue(true);
